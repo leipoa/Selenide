581057b52578a319e59b3de19f80dbfce1f36674
Darya Vylomova <qwerty6108@gmail.com>
2023-11-15 20:10:37 +0300

    correct tests

--- src/test/java/FormTests.java
+++ src/test/java/FormTests.java
@@ -1,34 +1,42 @@
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 
+
 public class FormTests {
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
+    }
+    String planningDate = generateDate(4);
 
     @Test
     void correctForm(){
         open("http://localhost:9999");
         $$(".input__control").first().setValue("Челябинск");
-        $("[data-test-id=date] input").click();
-        $("[data-test-id=date] input").clear();
-        $("[data-test-id=date] input").setValue("22.11.2023");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Выломова Дарья");
         $("[data-test-id=phone] input").setValue("+79000000000");
         $("[data-test-id=agreement]").click();
         $("button.button").click();
         $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
-
-
-
-
-
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
 
     }
+
 
 
 }
